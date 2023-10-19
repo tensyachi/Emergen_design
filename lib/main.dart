dab117301d4bf926eea8824f4c5e5c06afdfb5b6
tensyachi <leeyc2006@gmail.com>
2023-10-20 02:50:20 +0900

    저장

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
       MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, //디버그표시 제거
       home: MyApp()
   )
   );
@@ -16,130 +16,83 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-        floatingActionButton: Builder(
+        floatingActionButton: Builder( //플로팅 버튼 생성
             builder: (context) {
-              return FloatingActionButton(child: Icon(Icons.reorder), onPressed: (){
-                showModalBottomSheet(context: context,
-                  builder: (context){
-                  return Column(
-                    children: [
-                      Flexible(
-                          child: Container(height: 250,
-                          decoration: BoxDecoration(
-                              color: Colors.black,
-                              borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
-                          ),
-                      )
-                      ),
-                      Flexible(flex: 1,
-                          child: Container(decoration: BoxDecoration(color: Colors.white),
-                            child: Column(children: [
-                              ListTile(
-                                  contentPadding: EdgeInsets.fromLTRB(10, 10, 5, 0),
-                                  horizontalTitleGap: 25,
-                                  textColor: Colors.black,
-                                  iconColor: Colors.black,
-                                  leading: Icon(Icons.assignment, size: 40),
-                                  title: Text('수락 환자 목록', style: TextStyle(fontSize: 20),),
-                                onTap: (){
-                                  Navigator.push(context,
-                                    PageRouteBuilder(
-                                      pageBuilder: (c, a1, a2) => Accepted(),
-                                      transitionsBuilder: (c, a1, a2, child) => FadeTransition(opacity: a1, child: child),
-                                      transitionDuration: Duration(milliseconds : 500),
-                                    ),
-                                  );
-                                },
-                              ),
-                              ListTile(
-                                contentPadding: EdgeInsets.fromLTRB(10, 10, 5, 0),
-                                horizontalTitleGap: 25,
-                                textColor: Colors.black,
-                                iconColor: Colors.black,
-                                leading: Icon(Icons.settings, size: 40),
-                                title: Text('설정', style: TextStyle(fontSize: 20),),
-                                onTap: (){
-                                  Navigator.push(context,
-                                    PageRouteBuilder(
-                                      pageBuilder: (c, a1, a2) => Accepted(),
-                                      transitionsBuilder: (c, a1, a2, child) => FadeTransition(opacity: a1, child: child),
-                                      transitionDuration: Duration(milliseconds : 500),
-                                    ),
-                                  );
-                                },
-                              )
-                            ]
-                            )
-                          )
-                      )
-                    ],
-                  );
-                  },
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
+              return FloatingActionButton(child: Icon(Icons.assignment), onPressed: (){ //누르면 수락 환자 목록 뜸
+                Navigator.push(context,
+                  PageRouteBuilder(
+                    pageBuilder: (c, a1, a2) => Accepted(),
+                    transitionsBuilder: (c, a1, a2, child) => SlideTransition( //오른쪽에서 왼쪽 슬라이드
+                        position: Tween(
+                          begin: Offset(-1.0, 0.0),
+                          end: Offset(0.0, 0.0)
+                        ).animate(a1),
+                      child: child
+                    ),
+                    transitionDuration: Duration(milliseconds : 1000),
                   ),
-                  constraints: const BoxConstraints(
-                      minHeight: 100,
-                      maxHeight: 700
-                  ),
-                  enableDrag: true,
-                  isScrollControlled: true,
                 );
               }
               );
             }
-            ),
-        appBar:AppBar(backgroundColor: Colors.black, elevation: 0,
+        ),
+        appBar:AppBar(backgroundColor: Colors.black, elevation: 0, //앱바 검은색
           title: Text('EmergenShare', style: TextStyle(fontSize: 40)),
         ),
-        body: ListView.builder(
-            itemCount: 5,
+        body: ListView.builder( //미수락 환자 목록
+            itemCount: 6,
             itemBuilder: (c, i){
-              return Container(
-                decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Colors.black, width: 3))
-                ),
-                height: 150,
-                child: Column( crossAxisAlignment: CrossAxisAlignment.start,
-                children: [ Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                        margin: EdgeInsets.all(10),
-                        child: Text('${i+1}번 환자   나이: ${34-3*i}세', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600))
-                    ),
-                    TextButton(onPressed: (){
-
-                    }, style: ButtonStyle(overlayColor: MaterialStateColor.resolveWith((states) => Colors.transparent),),
-                        child: Icon(Icons.arrow_right_alt, size: 40, color: Colors.black))
-                  ]
-                ),
-                  SizedBox(height: 50, child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
+              return Card(elevation: 4,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  child: Container(
+                      height: 150,
+                      child: Column( crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [ Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                    margin: EdgeInsets.all(10),
+                                    child: Text('${i+1}번 환자   나이: ${34-3*i}세', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600))
+                                ),
+                                TextButton(onPressed: (){ //상세 정보 페이지 띄우기
+                                  Navigator.push(context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (c, a1, a2) => Detail(),
+                                      transitionsBuilder: (c, a1, a2, child) => FadeTransition(opacity: a1, child: child),
+                                      transitionDuration: Duration(milliseconds : 500),
+                                    ),
+                                  );
+                                  }, style: ButtonStyle(overlayColor: MaterialStateColor.resolveWith((states) => Colors.transparent),), //버튼 클릭 애니메이션 제거
+                                    child: Icon(Icons.arrow_right_alt, size: 40, color: Colors.black))
+                              ]
+                          ),
+                            SizedBox(height: 50, child: SingleChildScrollView( //환자 증상 태그들
+                              scrollDirection: Axis.horizontal,
+                              child: Row(
                       children: [
-                        Tag('출혈'),
-                        Tag('골절'),
-                        Tag('절단'),
-                        Tag('복통'),
-                        Tag('구토'),
-                        Tag('어지럼증'),
-                        Tag('가려움'),
-                        Tag('타박상'),
-                        Tag('발작'),
-                        Tag('두드러기'),
-                        Tag('호흡곤란'),
-                        Tag('자상')
+                                Tag('출혈'),
+                                Tag('골절'),
+                                Tag('절단'),
+                                Tag('복통'),
+                                Tag('구토'),
+                                Tag('어지럼증'),
+                                Tag('가려움'),
+                                Tag('타박상'),
+                                Tag('발작'),
+                                Tag('두드러기'),
+                                Tag('호흡곤란'),
+                                Tag('자상')
                       ]
-                    ),
-                  ),
-                  ),
-                  Container(margin: EdgeInsets.fromLTRB(20, 0, 0, 0), child: Color(i))
-                ]
-                )
+                              ),
+                            ),
+                            ),
+                            Container(margin: EdgeInsets.fromLTRB(20, 0, 0, 0), child: Color(i))
+                          ]
+                      )
+                  )
               );
             }
-        )
-      );
+            )
+    );
   }
 }
 
@@ -174,7 +127,7 @@ class Color extends StatelessWidget {
       case 4:
         return Text('KTAS: ${a + 1}단계', style: TextStyle(color: Colors.green, fontSize: 25, fontWeight: FontWeight.w600));
       default:
-        return Text('KTAS: ${a + 1}단계', style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.w600));
+        return Text('KTAS: 5단계', style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.w600));
     }
   }
 }
@@ -183,8 +136,69 @@ class Accepted extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.black, elevation: 0, title: Text('')),
+      body: ListView.builder(
+          itemCount: 6,
+          itemBuilder: (c, i){
+            return Card(elevation: 4,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                child: Container(
+                    height: 150,
+                    child: Column( crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [ Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                  margin: EdgeInsets.all(10),
+                                  child: Text('${i+1}번 환자   나이: ${34-3*i}세', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600))
+                              ),
+                              TextButton(onPressed: (){
+
+                              }, style: ButtonStyle(overlayColor: MaterialStateColor.resolveWith((states) => Colors.transparent),),
+                                  child: Icon(Icons.arrow_right_alt, size: 40, color: Colors.black))
+                            ]
+                        ),
+                          SizedBox(height: 50, child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                                children: [
+                                  Tag('출혈'),
+                                  Tag('골절'),
+                                  Tag('절단'),
+                                  Tag('복통'),
+                                  Tag('구토'),
+                                  Tag('어지럼증'),
+                                  Tag('가려움'),
+                                  Tag('타박상'),
+                                  Tag('발작'),
+                                  Tag('두드러기'),
+                                  Tag('호흡곤란'),
+                                  Tag('자상')
+                                ]
+                            ),
+                          ),
+                          ),
+                          Container(margin: EdgeInsets.fromLTRB(20, 0, 0, 0), child: Color(i))
+                        ]
+                    )
+                )
+            );
+          }
+      )
+    );
   }
 }
+class Detail extends StatelessWidget {
+  const Detail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.black, elevation: 0, ),
+      body: Container()
+    );
+  }
+}
+
 
 
